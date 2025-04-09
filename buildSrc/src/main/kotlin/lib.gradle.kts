d801629d6b9328369da2c5e766b1a28e731af70f
Oleg Sobolev <sobolev_ov@mail.ru>
2025-04-10 01:54:23 +0200

    Move to multi-module project

---
+++ buildSrc/src/main/kotlin/lib.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    `java-library`
+}
+
+group = "io.github.osobolev.karaed"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+sourceSets {
+    main {
+        java.srcDir("src")
+        resources.srcDir("resources")
+    }
+    test {
+        java.srcDir("unit")
+        resources.srcDir("unitResources")
+    }
+    create("manual") {
+        java.srcDir("test")
+        resources.srcDir("testResources")
+    }
+}
+
+configurations["manualImplementation"].extendsFrom(configurations["implementation"])
+configurations["manualRuntimeOnly"].extendsFrom(configurations["runtimeOnly"])
+configurations["manualCompileOnly"].extendsFrom(configurations["compileOnly"])
+
+dependencies {
+    "manualImplementation"(sourceSets["main"].output)
+}
+
+tasks {
+    withType(JavaCompile::class) {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+        options.compilerArgs.add("-Xlint:deprecation")
+    }
+}
---
+++ gui-manual/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("lib")
+}
+
+dependencies {
+    implementation("io.github.java-diff-utils:java-diff-utils:4.15")
+    implementation("com.google.code.gson:gson:2.12.1")
+}
