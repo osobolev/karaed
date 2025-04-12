1de3619d6a7f082455e6ba7f0b0de6eee3c05e8e
Oleg Sobolev <sobolev_ov@mail.ru>
2025-04-12 02:18:19 +0200

    Minor fixes

--- subprojects/gui/build.gradle.kts
+++ subprojects/gui/build.gradle.kts
@@ -3,10 +3,6 @@ plugins {
 }
 
 dependencies {
-    api("com.google.code.gson:gson:2.12.1")
-
-    implementation("io.github.java-diff-utils:java-diff-utils:4.15")
-    implementation(project(":run-util"))
     implementation(project(":json-util"))
     implementation(project(":workdir"))
     implementation(project(":engine"))
