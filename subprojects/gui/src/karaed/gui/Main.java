7910bc120ee5c330726a10ce1454718819dc4ebd
Oleg Sobolev <sobolev_ov@mail.ru>
2025-07-19 20:50:49 +0200

    Parsing command line args

--- subprojects/gui/src/karaed/gui/Main.java
+++ subprojects/gui/src/karaed/gui/Main.java
@@ -10,8 +10,32 @@ import karaed.tools.Tools;
 import javax.swing.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Main {
+
+    private record Args(String rootDir, List<String> args) {}
+
+    private static Args parseArgs(String[] args) {
+        String rootDir = null;
+        List<String> other = new ArrayList<>();
+        int i = 0;
+        while (i < args.length) {
+            String arg = args[i++];
+            if (arg.startsWith("-")) {
+                String option = arg.substring(1);
+                if ("r".equals(option)) {
+                    if (i < args.length) {
+                        rootDir = args[i++];
+                    }
+                }
+            } else {
+                other.add(arg);
+            }
+        }
+        return new Args(rootDir, other);
+    }
 
     public static void main(String[] args) {
         ProcUtil.registerShutdown();
@@ -22,12 +46,17 @@ public final class Main {
             Path.of("C:\\Users\\sobol\\.jkara\\python"),
             Path.of("C:\\Users\\sobol\\.jkara\\ffmpeg\\bin")
         );
-        Path rootDir = Path.of("C:\\home\\projects\\my\\karaed");
 
+        Args pargs = parseArgs(args);
         SwingUtilities.invokeLater(() -> {
+            if (pargs.rootDir == null) {
+                ShowMessage.error(null, "No root directory specified");
+                return;
+            }
+            Path rootDir = Path.of(pargs.rootDir);
             Thread.currentThread().setUncaughtExceptionHandler((t, ex) -> logger.error(ex));
-            if (args.length > 0) {
-                Path path = Path.of(args[0]);
+            if (!pargs.args.isEmpty()) {
+                Path path = Path.of(pargs.args.getFirst());
                 Path dir;
                 if (Files.isDirectory(path)) {
                     dir = path;
