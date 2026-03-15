568aefbe42edb6de8de458c729b4d8bbb149f35d
Oleg Sobolev <sobolev_ov@mail.ru>
2026-03-15 18:34:23 +0100

    Java memory size

--- config/karaed.bat
+++ config/karaed.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" "%~dp0jre\bin\javaw" -client -jar "%~dp0gui-1.0.jar" -r "%~dp0." %* && exit 0 || exit 1
+start "" "%~dp0jre\bin\javaw" -client -Xmx512m -jar "%~dp0gui-1.0.jar" -r "%~dp0." %* && exit 0 || exit 1
