c240fccb125bceac5dde6ffcff1ea64d79beeb28
Oleg Sobolev <sobolev_ov@mail.ru>
2026-03-22 18:26:41 +0100

    Remove version number

--- subprojects/distr/config/karaed.bat
+++ subprojects/distr/config/karaed.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" "%~dp0..\runtime\bin\javaw" -Xmx512m -Dapp.rootDir="%~dp0." -jar "%~dp0gui-1.0.jar" %* && exit 0 || exit 1
+start "" "%~dp0..\runtime\bin\javaw" -Xmx512m -Dapp.rootDir="%~dp0." -jar "%~dp0gui.jar" %* && exit 0 || exit 1
