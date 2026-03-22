f4cf38999670d74878fec82257d612539c6227dc
Oleg Sobolev <sobolev_ov@mail.ru>
2026-03-22 23:51:37 +0100

    No need for exit code

--- subprojects/distr/config/karaed.bat
+++ subprojects/distr/config/karaed.bat
@@ -1,2 +1,2 @@
 @echo off
-start "" "%~dp0..\runtime\bin\javaw" @"%~dp0options.args" -Dapp.rootDir="%~dp0." -jar "%~dp0gui.jar" %* && exit 0 || exit 1
+start "" "%~dp0..\runtime\bin\javaw" @"%~dp0options.args" -Dapp.rootDir="%~dp0." -jar "%~dp0gui.jar" %*
