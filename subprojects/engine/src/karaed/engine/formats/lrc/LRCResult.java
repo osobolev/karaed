8e828a3544f06af06e435ff48e299aafc91127fd
Oleg Sobolev <sobolev_ov@mail.ru>
2026-04-07 11:34:44 +0200

    Add other LRC fields

--- subprojects/engine/src/karaed/engine/formats/lrc/LRCResult.java
+++ subprojects/engine/src/karaed/engine/formats/lrc/LRCResult.java
@@ -1,5 +1,9 @@
 package karaed.engine.formats.lrc;
 
 public record LRCResult(
+    String artistName,
+    String albumName,
+    String trackName,
+    Double duration,
     String plainLyrics
 ) {}
