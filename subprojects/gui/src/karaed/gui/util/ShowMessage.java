08712f5e3375b65ec6de06d2246509500984530a
Oleg Sobolev <sobolev_ov@mail.ru>
2026-09-18 17:56:49 +0200

    Showing messages for components different from Windows looks weird

--- subprojects/gui/src/karaed/gui/util/ShowMessage.java
+++ subprojects/gui/src/karaed/gui/util/ShowMessage.java
@@ -3,22 +3,22 @@ package karaed.gui.util;
 import karaed.gui.ErrorLogger;
 
 import javax.swing.*;
-import java.awt.Component;
+import java.awt.Window;
 
 public final class ShowMessage {
 
-    public static void error(ErrorLogger logger, Component comp, Throwable ex) {
+    public static void error(ErrorLogger logger, Window window, Throwable ex) {
         logger.error(ex);
-        error(comp, ex.toString());
+        error(window, ex.toString());
     }
 
-    public static void error(Component comp, String message) {
-        JOptionPane.showMessageDialog(comp, message, "Error", JOptionPane.ERROR_MESSAGE);
+    public static void error(Window window, String message) {
+        JOptionPane.showMessageDialog(window, message, "Error", JOptionPane.ERROR_MESSAGE);
     }
 
-    public static boolean confirm2(Component comp, String message) {
+    public static boolean confirm2(Window window, String message) {
         int ans = JOptionPane.showConfirmDialog(
-            comp, message, "Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE
+            window, message, "Warning", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE
         );
         return ans == JOptionPane.YES_OPTION;
     }
